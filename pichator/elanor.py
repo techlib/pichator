51a534c6a7ac71a978aa613a2bb15ecda30ca6a1
Jakub Koudelka <koudelka.j@gmail.com>
2019-05-27 09:03:38 +0200

    Parse date properly in elanor
    Signed-off-by: Jakub Koudelka <koudelka.j@gmail.com>

--- pichator/elanor.py
+++ pichator/elanor.py
@@ -2,18 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from twisted.python import log
-from datetime import date
+from datetime import date, datetime
 import xml.etree.ElementTree as ET
 
 __all__ = ['Elanor']
 
 
-def oracle_date_to_date(O_date):
-    if O_date and O_date != 'None':
-        split_string = O_date.split('-')
-        return date(int(split_string[0]), int(split_string[1]), int(split_string[2]))
-    else:
-        return ""
+def parse_date(date_str):
+    return datetime.strptime(date_str, '%Y-%m-%d').date()
 
 
 class Elanor:
@@ -29,16 +25,16 @@ class Elanor:
             xml = ET.fromstring(pv.dalsi1_xml)
 
             for pv_item in xml.findall('.//uv_sjed_tyd'):
-                date_from = oracle_date_to_date(pv_item.attrib['datum_od'])
-                date_to = oracle_date_to_date(pv_item.attrib['datum_do'])
+                date_from = parse_date(pv_item.attrib['datum_od'])
+                date_to = parse_date(pv_item.attrib['datum_do'])
                 occupancy = round(float(pv_item.attrib['hodnota']) / 40, 2)
 
                 retval.append({
                     'pvid': pv.oscpv,
                     'occupancy': occupancy,
                     'department': pv.kod,
-                    'validity': (max(oracle_date_to_date(pv.dat_nast), date_from),
-                                 min(oracle_date_to_date(pv.dat_ukon), date_to)),
+                    'validity': (max(parse_date(pv.dat_nast), date_from),
+                                 min(parse_date(pv.dat_ukon), date_to)),
                     'emp_no': emp_no
                 })
 
