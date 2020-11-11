7ea982869cb2c67dd0d3799fc7f258e40593a973
Lea <lea.bradacova@techlib.cz>
2020-11-11 13:33:51 +0100

    xlsx minor corrections

--- pichator/site/xlsx_export.py
+++ pichator/site/xlsx_export.py
@@ -14,10 +14,8 @@ def xlsx_export(data, dept, month, year, **kwargs):
   bold = workbook.add_format({'bold': True})
 
   if len(data) > 0:
-
     worksheet.write('A1', doc_name, bold)
     worksheet.write('A3', period)
-
 
     # Column name
     worksheet.write('A5', 'Číslo zaměstnance', bold)
@@ -30,22 +28,18 @@ def xlsx_export(data, dept, month, year, **kwargs):
     row = 6
     col = 2
     for emp in data:
-      worksheet.write(row, 0, emp['pvid']) #pracovni pomer
-      worksheet.write(row, 1, emp['name']) #jmeno
+      worksheet.write(row, 0, emp['pvid'])
+      worksheet.write(row, 1, emp['name'])
       for day in range(1, len(data[0])-1):
         worksheet.write(row, col, emp.get(str(day), ''))
         col += 1
       col = 2
       row += 1
   else:
-    # nejsou data
     worksheet.write('A5', 'Prázdná odpověď', bold)
     worksheet.write('A6', 'Dotaz vrátil prázdný záznam.')
     worksheet.write('A7', 'Žádný zaměstnanec tohoto útvaru nemá platný PV, nebo vyplněnou pracovní dobu.')
     worksheet.write('A8', 'Pokud si myslíte, že se jedná o chybu, kontaktujte prosím mailto:helpdeskict@techlib.cz')
-
-
-
 
   workbook.close()
   output.seek(0)
